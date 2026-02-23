53b3052d5b5dece0d312dce46de47ce7e4067845
Fernando Pérez Alonso-Castrillo <nanoperezac@gmail.com>
2026-02-23 18:53:22 +0100

    Create links.py

---
+++ scripts/links.py
@@ -0,0 +1,19 @@
+nombre_archivo = input("Escribe el nombre del archivo XML: ")
+
+with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+    contenido = archivo.read()
+
+palabras = contenido.split()
+
+# Lista para guardar enlaces
+enlaces = []
+
+for palabra in palabras:
+    if palabra.startswith("http://") or palabra.startswith("https://"):
+        # Limpiar posibles caracteres finales
+        enlace = palabra.strip(".,;:()[]\"'") # quitar signos de puntuación
+        enlaces.append(enlace)
+
+print("Enlaces encontrados en el artículo:")
+for enlace in enlaces:
+    print(enlace)
