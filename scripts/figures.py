54d55dc95447f01ee0a3a7b7039694e842f0c5f5
Fernando Pérez Alonso-Castrillo <nanoperezac@gmail.com>
2026-02-23 18:24:46 +0100

    Create figures.py

---
+++ scripts/figures.py
@@ -0,0 +1,8 @@
+nombre_archivo = input("Escribe el nombre del archivo XML: ")
+
+with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+    contenido = archivo.read()
+
+num_figuras = contenido.count("<fig>")
+
+print("Número de figuras en el artículo:", num_figuras)
