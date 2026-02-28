5c1c80dcd0458224d5085c1af990bcae321d79b3
Fernando Pérez Alonso-Castrillo <nanoperezac@gmail.com>
2026-02-28 18:34:09 +0100

    Update figures.py

--- scripts/figures.py
+++ scripts/figures.py
@@ -3,6 +3,6 @@ nombre_archivo = input("Escribe el nombre del archivo XML: ")
 with open(nombre_archivo, "r", encoding="utf-8") as archivo:
     contenido = archivo.read()
 
-num_figuras = contenido.count("<fig>")
+num_figuras = contenido.count("<figure")
 
 print("Número de figuras en el artículo:", num_figuras)
