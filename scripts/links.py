f68f45eacf1debb6f3517c2a92a4609c4eb5e1bc
Fernando Pérez Alonso-Castrillo <nanoperezac@gmail.com>
2026-03-08 12:43:55 +0100

    Update links.py

--- scripts/links.py
+++ scripts/links.py
@@ -1,31 +1,24 @@
 import os
+import re
 
 papers = "../papers"
-xmls = [f for f in os.listdir(papers) if f.endswith(".xml")]
+xml_files = [f for f in os.listdir(papers) if f.endswith(".xml")]
 
 with open("../resultados/enlaces.txt", "w", encoding="utf-8") as out:
 
-    for nombre_archivo in xmls:
+    for nombre_archivo in xml_files:
 
         with open(f"{papers}/{nombre_archivo}", "r", encoding="utf-8") as archivo:
             text = archivo.read()
 
         out.write(f"{nombre_archivo}:\n")
 
-        i = 0
-        while True:
-            i = text.find('target="', i)
-            if i == -1:
-                break
-            i += len('target="')
-            j = text.find('"', i)
-            link = text[i:j]
+        enlaces = set(re.findall(r'https?://[^\s<>"\]]+', text))
 
-            if link.startswith("http") and link != "https://github.com/kermitt2/grobid":
+        for link in enlaces:
+            if link != "https://github.com/kermitt2/grobid":
                 out.write(link + "\n")
-
-            i = j
 
         out.write("\n")
 
-print("Enlaces guardados en 'enlaces.txt'")
+print("Enlaces guardados en '../resultados/enlaces.txt'")
