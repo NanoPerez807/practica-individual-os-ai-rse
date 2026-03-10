a790a571cadfa1d7561fcf22ff612545e0e6f19d
Fernando Pérez Alonso-Castrillo <nanoperezac@gmail.com>
2026-03-10 10:54:36 +0100

    Update links.py

--- scripts/links.py
+++ scripts/links.py
@@ -16,8 +16,8 @@ with open("../resultados/enlaces.txt", "w", encoding="utf-8") as out:
         enlaces = set(re.findall(r'https?://[^\s<>"\]]+', text))
 
         for link in enlaces:
-            if link != "https://github.com/kermitt2/grobid" and link != "https://raw.githubusercontent.com/kermitt2/grobid/master/grobid-home/schemas/xsd/Grobid.xsd" and link != "http://www.tei-c.org/ns/1.0" and link !>                out.write(link + "\n")
-
+            if link != "https://github.com/kermitt2/grobid" and link != "https://raw.githubusercontent.com/kermitt2/grobid/master/grobid-home/schemas/xsd/Grobid.xsd" and link != "http://www.tei-c.org/ns/1.0" and link != "http://www.w3.org/2001/XMLSchema-instance" and link != "http://www.w3.org/1999/xlink":
+                out.write(link + "\n")
         out.write("\n")
 
 print("Enlaces guardados en 'enlaces.txt'")
